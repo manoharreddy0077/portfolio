11c0c2e05cd8c390710e190386469817dc2d1d4b
manoharreddy0077 <smanoharreddy1739@gmail.com>
2024-07-30 09:37:41 +0530

    made about responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
-// tailwind.config.js
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  content: [
+    "./index.html",  // Adjust based on the location of your HTML file
+    "./scripts.js",  // Include JS files if you're dynamically adding classes
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
